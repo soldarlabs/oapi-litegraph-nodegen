e3ed11e7648ab517a01e50216529cf31133ec8ab
Rick Staa <rick.staa@outlook.com>
2024-11-21 09:38:02 +0100

    refactor: replace NodeGenerator with class
    This commit replaces the NodeGenerator constructor function + factory
    implementation with a class.

--- src/OpenAPINode.ts
+++ src/OpenAPINode.ts
@@ -1,45 +1,51 @@
 /**
- * @file Contains the constructor function for the OpenAPI nodes.
+ * @file Contains the class definition for the OpenAPI nodes.
  */
-import { LiteGraph } from "litegraph.js";
+import { LGraphNode } from "litegraph.js";
 
 /**
- * Constructor function for OpenAPI nodes. It creates inputs/outputs and other node
- * properties based on the OpenAPI operation.
- * @param method - HTTP method of the operation.
- * @param path - Path of the operation.
- * @param operation - The OpenAPI operation object.
+ * Class for OpenAPI nodes. It creates inputs/outputs and other node properties based
+ * on the OpenAPI operation.
  */
-function OpenAPINode(this: any, method: string, path: string, operation: any) {
-  // Create inputs based on operation parameters.
-  if (operation.parameters) {
-    operation.parameters.forEach((param: any) => {
-      const paramType = param.schema ? param.schema.type : "string";
-      this.addInput(`${param.name} (${paramType})`, paramType);
-    });
-  }
+export class OpenAPINode extends LGraphNode {
+  /**
+   * Creates an instance of OpenAPINode.
+   * @param title - The title of the node.
+   * @param operation - The OpenAPI operation object.
+   */
+  constructor(title: string, operation: any) {
+    super(title);
 
-  // Create inputs based on operation request body.
-  if (operation.requestBody && operation.requestBody.content) {
-    const content = operation.requestBody.content;
-    for (const contentType in content) {
-      const schema = content[contentType].schema;
-      if (schema && schema.properties) {
-        for (const propName in schema.properties) {
-          const propType = schema.properties[propName].type || "string";
-          this.addInput(`${propName} (${propType})`, propType);
+    // Create inputs based on operation parameters.
+    if (operation.parameters) {
+      operation.parameters.forEach((param: any) => {
+        const paramType = param.schema ? param.schema.type : "string";
+        this.addInput(`${param.name} (${paramType})`, paramType);
+      });
+    }
+
+    // Create inputs based on operation request body.
+    if (operation.requestBody && operation.requestBody.content) {
+      const content = operation.requestBody.content;
+      for (const contentType in content) {
+        const schema = content[contentType].schema;
+        if (schema && schema.properties) {
+          for (const propName in schema.properties) {
+            const propType = schema.properties[propName].type || "string";
+            this.addInput(`${propName} (${propType})`, propType);
+          }
         }
       }
     }
-  }
 
-  // Create output based on operation response.
-  const responses = operation.responses;
-  if (responses && responses['200'] && responses['200'].content) {
-    const responseType = responses['200'].content['application/json'].schema.type;
-    this.addOutput("Response", responseType);
-  } else {
-    this.addOutput("Response", "string");
+    // Create output based on operation response.
+    const responses = operation.responses;
+    if (responses && responses['200'] && responses['200'].content) {
+      const responseType = responses['200'].content['application/json'].schema.type;
+      this.addOutput("Response", responseType);
+    } else {
+      this.addOutput("Response", "string");
+    }
   }
 }
 
@@ -51,13 +57,10 @@ function OpenAPINode(this: any, method: string, path: string, operation: any) {
  * @returns A custom node class.
  */
 export function createOpenAPINodeClass(method: string, path: string, operation: any) {
-  const NodeClass = function () {
-    OpenAPINode.call(this, method, path, operation);
+  const title = `${method.toUpperCase()} ${path}`;
+  return class extends OpenAPINode {
+    constructor() {
+      super(title, operation);
+    }
   };
-  NodeClass.title = `${method.toUpperCase()} ${path}`;
-  NodeClass.prototype = Object.create(OpenAPINode.prototype);
-  NodeClass.prototype.constructor = NodeClass;
-  return NodeClass;
 }
-
-export { OpenAPINode };
