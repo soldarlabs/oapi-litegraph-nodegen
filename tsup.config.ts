04abafe644209fcbb0b6cf89fb220d2dc56605dc
Rick Staa <rick.staa@outlook.com>
2024-12-05 12:10:33 +0100

    refactor: simplify library configs
    This commit simplifies the tsconfig and tsup config since we are
    shipping a standalone library and some options are redundant when not
    bundling.

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,21 +7,18 @@ console.log(`Building for ${NODE_ENV}...`);
 
 export default defineConfig({
   entry: ["src/**/*.ts"],
-  // format: ["esm", "cjs"],
-  format: ["esm"],
-  target: "es2020",
+  format: ["esm", "cjs"],
   dts: true,
   sourcemap: !isProduction,
   clean: true,
   outDir: "dist",
   skipNodeModulesBundle: true,
-  splitting: true,
-  treeshake: isProduction,
   bundle: false,
   env: {
     NODE_ENV,
   },
-  // define: {
-  //   "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
-  // },
+  // Replace env variables to avoid undefined values in the bundle.
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+  },
 });
