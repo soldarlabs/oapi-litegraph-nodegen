03a9e5248994a51a8112ab6d0c8737cd9dcec223
Rick Staa <rick.staa@outlook.com>
2024-12-10 09:59:57 +0100

    fix(demo): fix theme provider reference
    This commit fixes the reference to the theme provider for which the naming
    was changed in a previous commit.

--- demo/src/hooks/use-theme.tsx
+++ demo/src/hooks/use-theme.tsx
@@ -2,7 +2,7 @@
  * @file Provides a hook for accessing the current theme context.
  */
 import { useContext } from "react";
-import { ThemeProviderContext } from "@/contexts/theme-context";
+import { ThemeContext } from "@/contexts/theme-context";
 
 /**
  * Hook for using the current theme context.
@@ -10,7 +10,7 @@ import { ThemeProviderContext } from "@/contexts/theme-context";
  * @throws Will throw an error if used outside of a ThemeProvider.
  */
 export const useTheme = () => {
-  const context = useContext(ThemeProviderContext);
+  const context = useContext(ThemeContext);
   if (!context) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
